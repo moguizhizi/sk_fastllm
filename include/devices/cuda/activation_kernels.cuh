896fb55184e557fe87279cdce75c02f2a3a45587
devil-son <moguizhiz@126.com>
2026-03-15 03:01:40 +0000

    use_vec

---
+++ include/devices/cuda/activation_kernels.cuh
@@ -0,0 +1,4 @@
+#include "fastllm.h"
+#include <cstdint>
+
+bool use_vec(uint32_t num_tokens, uint32_t elementSize, uint32_t num_elements);
--- src/devices/cuda/activation_kernels.cu
+++ src/devices/cuda/activation_kernels.cu
@@ -1,6 +1,7 @@
-#include <cstdint>
+
 #include <cuda_fp16.h>
 #include <cuda_bf16.h>
+#include "activation_kernels.cuh"
 
 #ifndef USE_ROCM
   #define FASTLLM_LDG(arg) __ldg(arg)
@@ -144,4 +145,31 @@ __global__ void act_and_mul_kernel(
       out_ptr[idx] = compute<scalar_t, ACT_FN, act_first>(x, y);
     }
   }
+}
+
+bool use_vec(uint32_t num_tokens, uint32_t elementSize, uint32_t num_elements) {
+    if (elementSize == 0) {
+        return false;
+    }
+
+    int device = -1;
+    cudaError_t err = cudaGetDevice(&device);
+    if (err != cudaSuccess) {
+        printf("cudaGetDevice failed: %s\n", cudaGetErrorString(err));
+        return false;
+    }
+
+    cudaDeviceProp prop;
+    auto error = cudaGetDeviceProperties(&prop, device);
+    if (error != cudaSuccess) {
+        printf("cudaGetDeviceProperties returned %d\n-> %s\n", (int)error, cudaGetErrorString(error));
+        return false;
+    }
+
+    int cc_major = prop.major;
+    int support_vec = (cc_major >= 10 && num_tokens > 128) ? 32 : 16;
+    int vec_size = support_vec / elementSize;
+    bool use_vec = (num_elements % vec_size == 0);
+
+    return use_vec;
 }
