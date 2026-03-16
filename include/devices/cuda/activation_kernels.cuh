a9e65d5d79a7d5f0dcc040f5cf46d573fed17f93
devil-son <moguizhiz@126.com>
2026-03-16 14:02:37 +0000

    gelu_and_mul

--- include/devices/cuda/activation_kernels.cuh
+++ include/devices/cuda/activation_kernels.cuh
@@ -2,5 +2,7 @@
 
 bool silu_and_mul(const fastllm::Data &input, fastllm::Data &output);
 bool mul_and_silu(const fastllm::Data &input, fastllm::Data &output);
+bool gelu_and_mul(const fastllm::Data &input, fastllm::Data &output);
+bool gelu_tanh_and_mul(const fastllm::Data &input, fastllm::Data &output);
 
 
