0cdb9de77a223fb1877e966507d9f466becab37b
devil-son <moguizhiz@126.com>
2026-03-29 09:30:25 +0000

    FASTLLM_DISPATCH_FLOAT_TYPES

--- src/devices/cuda/activation_kernels.cu
+++ src/devices/cuda/activation_kernels.cu
@@ -23,28 +23,6 @@
 #    define FASTLLM_LDG(arg) *(arg)
 #endif
 
-#define FASTLLM_DISPATCH_FLOAT_TYPES(TYPE, BODY) \
-    switch (TYPE) {                              \
-        case fastllm::DataType::FLOAT32: {       \
-            using scalar_t = float;              \
-            using packed_t = float2;             \
-            BODY;                                \
-            break;                               \
-        }                                        \
-        case fastllm::DataType::FLOAT16: {       \
-            using scalar_t = half;               \
-            using packed_t = __half2;            \
-            BODY;                                \
-            break;                               \
-        }                                        \
-        case fastllm::DataType::BFLOAT16: {      \
-            using scalar_t = __nv_bfloat16;      \
-            using packed_t = __nv_bfloat162;     \
-            BODY;                                \
-            break;                               \
-        }                                        \
-    }
-
 #define FASTLLM_ACT_MUL_BODY(ACT_FN, PACKED_ACT_FN, ACT_FIRST)                           \
     int input_len = input.Count(0);                                                      \
     int output_len = output.Count(0);                                                    \
