f7eab3c21f3f5ba108fd5adcc2a0484c29df324c
devil-son <moguizhiz@126.com>
2026-04-12 03:06:33 +0000

    new file

---
+++ src/devices/cuda/fastllm-vllm-cuda-common.cuh
@@ -0,0 +1,6 @@
+#include "fastllm.h"
+
+#define checkCudaErrors(message, val) showError(val, message, __FILE__, __LINE__)
+void showError(cudaError_t result, char const* const message, const char* const file, int const line);
+
+void *FastllmCudaPrepareInput(const fastllm::Data &input);
