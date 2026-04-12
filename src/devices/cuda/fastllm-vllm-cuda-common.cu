31b3a43d788bec7ef1381ec61759797941717c26
devil-son <moguizhiz@126.com>
2026-04-12 06:24:00 +0000

    modify file

--- src/devices/cuda/fastllm-vllm-cuda-common.cu
+++ src/devices/cuda/fastllm-vllm-cuda-common.cu
@@ -1,11 +1,22 @@
+#include <cuda_runtime.h>
 #include "fastllm-vllm-cuda-common.cuh"
+
+struct CudaMemoryBuffer {
+    void *data;
+    size_t size;
+    bool busy;
+
+    CudaMemoryBuffer() {}
+
+    CudaMemoryBuffer(void *data, size_t size, bool busy) : data(data), size(size), busy(busy) {}
+};
 
 void *FastllmCudaPrepareInput(const fastllm::Data &input) {
     void *ret;
     if (input.dataDevice == fastllm::DataDevice::CUDA) {
-        ret = (void*)input.cudaData;
+        ret = (void *)input.cudaData;
     } else {
-        ret = (void*)(input.expansionBytes);
+        ret = (void *)(input.expansionBytes);
         auto state = cudaMemcpy(ret, input.cpuData, input.expansionBytes, cudaMemcpyHostToDevice);
         if (cudaSuccess != state) {
             checkCudaErrors("Error: CUDA error when copy from memory to GPU!", state);
@@ -15,3 +26,85 @@ void *FastllmCudaPrepareInput(const fastllm::Data &input) {
     return ret;
 }
 
+void FastllmCudaFinishInput(const fastllm::Data &input, void *data) {
+    if (input.dataDevice != fastllm::DataDevice::CUDA) {
+        FastllmCudaFree(data);
+    }
+}
+
+int FastllmCudaGetDevice() {
+    int id = -1;
+    cudaGetDevice(&id);
+    return id;
+}
+
+void FastllmCudaSetDevice(int gpu_id) {
+    cudaSetDevice(gpu_id);
+}
+
+std::map<int, std::vector<CudaMemoryBuffer>> cudaBuffersMap;
+
+std::map<int, size_t> noBusyCnt;
+
+std::map<int, int> cudaBuffersMinId; // 最小的空闲id
+
+std::map<int, std::vector<CudaMemoryBuffer>> bigBuffersMap;
+
+void FastllmCudaFree(void *ret) {
+    if (ret == nullptr) {
+        return;
+    }
+    if (cudaBuffersMap.empty()) return;
+    int oriId = FastllmCudaGetDevice();
+    cudaError_t state = cudaSuccess;
+    for (auto &it : cudaBuffersMap) {
+        if (noBusyCnt[it.first] > 1024 * 1024 * 1024) {
+            auto &cudaBuffers = it.second;
+            std::vector<CudaMemoryBuffer> temp;
+            for (int i = 0; i < cudaBuffers.size(); i++) {
+                if (!cudaBuffers[i].busy) {
+                    state = cudaSetDevice(it.first);
+                    state = cudaFree(cudaBuffers[i].data);
+                    if (cudaSuccess != state) printf("Error: CUDA error when release memory on device %d!", it.first);
+                    checkCudaErrors("", state);
+                } else {
+                    temp.push_back(cudaBuffers[i]);
+                }
+            }
+            cudaBuffers.clear();
+            it.second = temp;
+            noBusyCnt[it.first] = 0;
+        }
+    }
+
+    for (auto &it : cudaBuffersMap) {
+        auto &cudaBuffers = it.second;
+        for (int i = 0; i < cudaBuffers.size(); i++) {
+            if (cudaBuffers[i].data == ret) {
+                noBusyCnt[it.first] += cudaBuffers[i].size;
+                cudaBuffers[i].busy = false;
+                cudaBuffersMinId[it.first] = std::min(cudaBuffersMinId[it.first], i);
+#ifdef CUDA_MEM_DEBUG
+                CudaMemDebugRemove(ret);
+#endif
+                return;
+            }
+        }
+        auto &bigBuffers = bigBuffersMap[it.first];
+        for (int i = 0; i < bigBuffers.size(); i++) {
+            if (bigBuffers[i].data == ret) {
+                bigBuffers[i].busy = false;
+#ifdef CUDA_MEM_DEBUG
+                CudaMemDebugRemove(ret);
+#endif
+                return;
+            }
+        }
+    }
+#ifdef CUDA_MEM_DEBUG
+    CudaMemDebugRemove(ret);
+#endif
+    state = cudaFree(ret);
+    FastllmCudaSetDevice(oriId);
+    checkCudaErrors("CUDA error when release memory!", state);
+}
--- src/devices/cuda/fastllm-vllm-cuda-common.cuh
+++ src/devices/cuda/fastllm-vllm-cuda-common.cuh
@@ -4,3 +4,8 @@
 void showError(cudaError_t result, char const* const message, const char* const file, int const line);
 
 void *FastllmCudaPrepareInput(const fastllm::Data &input);
+void FastllmCudaFinishInput(const fastllm::Data &input, void *data);
+void FastllmCudaFree(void *ret);
+
+int FastllmCudaGetDevice();
+void FastllmCudaSetDevice(int gpu_id);
